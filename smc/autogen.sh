d933bb45fa139c642c865e33d41de149464adf90
Tristan Heaven <tristanheaven@gmail.com>
2011-05-06 13:37:20 +0100

    provide a working "make dist"

--- smc/autogen.sh
+++ smc/autogen.sh
@@ -6,6 +6,8 @@ set -x # print commands
 case "x$1" in
 	x)
 		echo "Generating build system..."
+		mkdir -p m4
+		cp -f /usr/share/gettext/config.rpath .
 		if autoreconf --force --install --verbose $*; then
 			echo "Build system has been generated."
 		else
